60ae5cc1c87d1b642d751961c3974549d7595bae
viictorcamposs <campos.viictor@gmail.com>
2021-01-18 17:11:54 -0300

    update connection w/ aws

--- src/config/multer.js
+++ src/config/multer.js
@@ -38,7 +38,7 @@ const storageTypes = {
 
 module.exports = {
   dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-  storage: storageTypes[''],
+  storage: storageTypes['s3'],
   limits: {
     fileSize: 2 * 1024 * 1024,
   },
--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,13 @@ const multerConfig = require('./config/multer');
 const Post = require('./models/Post');
 
 routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
-  const { originalname: name, size, filename: key } = req.file;
+  const { originalname: name, size, key, location: url = '' } = req.file;
   
   const post = await Post.create({
     name,
     size, 
     key,
-    url: '',
+    url,
   });
   
   return res.json(post); 
