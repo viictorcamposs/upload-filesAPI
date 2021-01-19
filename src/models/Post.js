ed9e62f6d146375ed26b424e14809a37e31a7d12
viictorcamposs <campos.viictor@gmail.com>
2021-01-19 15:18:03 -0300

    removing files in local storage

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const aws = require('aws-sdk');
+const path = require('path');
+const { unlink } = require('fs');
+const { promisify } = require('util');
 
 const s3 = new aws.S3();
 
@@ -18,14 +21,14 @@ PostSchema.pre('save', function() {
   !this.url ? this.url = `${process.env.APP_URL}/files/${this.key}` : this.url = this.url
 }) ;
 
-PostSchema.pre('remove', function() {
+PostSchema.pre('remove', async function() {
   if(process.env.STORAGE_TYPE === 's3') {
     return s3.deleteObject({
       Bucket: 'uploadfiles-api',
       Key: this.key
     }).promise()
   } else {
-    
+    return promisify(unlink)(path.resolve(__dirname, '..', '..', 'tmp', 'uploads', this.key))
   }
 });
 
